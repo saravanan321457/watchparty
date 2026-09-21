1cbe8596ade95caee8f39ec7e42c8da9a52d4ae8
saravanangithubgit <saravanannew.in@gmail.com>
2026-09-21 18:03:17 +0530

    fix: add openrelay TURN servers for robust WebRTC traversal

--- lib/host_screen.dart
+++ lib/host_screen.dart
@@ -175,7 +175,11 @@ class _HostScreenState extends State<HostScreen> {
 
   Future<void> _createOffer() async {
     try {
-      _pc = await createPeerConnection({'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}]});
+      _pc = await createPeerConnection({'iceServers': [
+        {'urls': 'stun:stun.l.google.com:19302'},
+        {'urls': 'turn:openrelay.metered.ca:80', 'username': 'openrelayproject', 'credential': 'openrelayproject'},
+        {'urls': 'turn:openrelay.metered.ca:443?transport=tcp', 'username': 'openrelayproject', 'credential': 'openrelayproject'}
+      ]});
       _pc!.onConnectionState = (s) {
         if (s == RTCPeerConnectionState.RTCPeerConnectionStateFailed) {
           _safeSet(() => _status = 'Connection failed');
--- lib/viewer_screen.dart
+++ lib/viewer_screen.dart
@@ -124,7 +124,11 @@ class _ViewerScreenState extends State<ViewerScreen> {
 
   Future<RTCPeerConnection?> _ensurePeer() async {
     if (_pc != null) return _pc;
-    _pc = await createPeerConnection({'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}]});
+    _pc = await createPeerConnection({'iceServers': [
+      {'urls': 'stun:stun.l.google.com:19302'},
+      {'urls': 'turn:openrelay.metered.ca:80', 'username': 'openrelayproject', 'credential': 'openrelayproject'},
+      {'urls': 'turn:openrelay.metered.ca:443?transport=tcp', 'username': 'openrelayproject', 'credential': 'openrelayproject'}
+    ]});
     
     _pc!.onConnectionState = (s) {
       if (s == RTCPeerConnectionState.RTCPeerConnectionStateConnected) {
